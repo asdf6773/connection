af92dcaecde91772e083ee5f87fc53e056d2cfb3
asdf6773 <457776773@qq.com>
2017-03-29 01:06:06 +0800

    test

--- bundle.js
+++ bundle.js
@@ -53,34 +53,34 @@ window.show_coords = function(event) {
 var $ = require('jquery');
 var Particle = require('./particles.js');
 
-function load() {
 
-    document.addEventListener('touchstart', touch, false);
-    document.addEventListener('touchmove', touch, false);
-    document.addEventListener('touchend', touch, false);
 
-    function touch(event) {
-        var event = event || window.event;
+document.addEventListener('touchstart', touch, false);
+document.addEventListener('touchmove', touch, false);
+document.addEventListener('touchend', touch, false);
 
-        var oInp = document.getElementById("inp");
+function touch(event) {
+    var event = event || window.event;
 
-        switch (event.type) {
-            case "touchstart":
-                oInp.innerHTML = "Touch started (" + event.touches[0].clientX + "," + event.touches[0].clientY + ")";
-                break;
-            case "touchend":
-                oInp.innerHTML = "<br>Touch end (" + event.changedTouches[0].clientX + "," + event.changedTouches[0].clientY + ")";
-                break;
-            case "touchmove":
-                event.preventDefault();
-                oInp.innerHTML = "<br>Touch moved (" + event.touches[0].clientX + "," + event.touches[0].clientY + ")";
-                break;
-        }
-        alert(1222);
+    var oInp = document.getElementById("inp");
 
+    switch (event.type) {
+        case "touchstart":
+            oInp.innerHTML = "Touch started (" + event.touches[0].clientX + "," + event.touches[0].clientY + ")";
+            break;
+        case "touchend":
+            oInp.innerHTML = "<br>Touch end (" + event.changedTouches[0].clientX + "," + event.changedTouches[0].clientY + ")";
+            break;
+        case "touchmove":
+            event.preventDefault();
+            oInp.innerHTML = "<br>Touch moved (" + event.touches[0].clientX + "," + event.touches[0].clientY + ")";
+            break;
     }
+
+
 }
-window.addEventListener('load', load, false);
+
+
 //var THREE = require('three');
 var vShader = $('#vertexshader');
 var fShader = $('#fragmentshader');
